6ad120e8de4777c9c6ee6d5687417e34849887d3
Defixer <jp.mendoza017@gmail.com>
2018-10-31 14:01:24 +0800

    Refactor check_sugar_imports to be main function

--- check_sugar_imports.py
+++ check_sugar_imports.py
@@ -1,6 +1,14 @@
-from check_users_import_file import check_users
+import pandas as pd
+from datetime import datetime
+from check_users_files import check_users
+import utilities
 
 def main():
-	check_users()
+	current_date = datetime.today().strftime('%Y%m%d')
+	output_file = "{}_import_results.csv".format(current_date)
+
+	import_report_df = check_users()
+	# import_report_df = check_contact_roles(import_report_df)
+	utilities.df_to_csv(utilities.import_report_df, output_file)
 
 main()
